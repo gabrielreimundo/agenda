03d560936617e3d7db19993d9abb1d86d0b1aa16
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2024-02-22 20:41:20 -0300

    at

--- home/home.js
+++ home/home.js
@@ -1,4 +1,7 @@
 const dgvDados = document.getElementById('dgvDados')
+const resPesquisa = document.getElementById('resultado');
+let TitleOfDgvExist=false;
+
 
 const createDataGridView = function(el, dgvDados) {
   const dgvLinha = document.createElement("div");
@@ -58,7 +61,42 @@ fetch('http://localhost:8081/dados')
 })
 
 
+const createTitleOfDgv=function(resPesquisa){
+  TitleOfDgvExist=true
+  resPesquisa.innerHTML="Resultado da Pesquisa: "
 
+  const dgvContatos=document.createElement('div')
+  dgvContatos.setAttribute('id','dgvContatos')
+
+  const dgvTitulos=document.createElement('div')
+  dgvTitulos.setAttribute('class','dgvTitulos')
+
+  const c1=document.createElement('div')
+  c1.setAttribute('class','c1')
+  c1.innerHTML="Id";
+  dgvTitulos.appendChild(c1)
+  const c2=document.createElement('div')
+  c2.setAttribute('class','c2')
+  c2.innerHTML="Nome";
+  dgvTitulos.appendChild(c2)
+  const c3=document.createElement('div')
+  c3.setAttribute('class','c3')
+  c3.innerHTML="Celular";
+  dgvTitulos.appendChild(c3)
+  const c4=document.createElement('div')
+  c4.setAttribute('class','c4')
+  c4.innerHTML="E-mail";
+  dgvTitulos.appendChild(c4)
+  const c5=document.createElement('div')
+  c5.setAttribute('class','c5')
+  c5.innerHTML="Data de Nascimento";
+  dgvTitulos.appendChild(c5)
+  
+  dgvContatos.appendChild(dgvTitulos)
+  resPesquisa.appendChild(dgvContatos)
+
+
+}
 
 document.getElementById('BtnPesquisa').addEventListener('click', function(e) {
   e.preventDefault();
@@ -72,79 +110,81 @@ document.getElementById('BtnPesquisa').addEventListener('click', function(e) {
   })
   .then(response => response.json())
   .then(data =>{
-    const resPesquisa = document.getElementById('resultado');
-    resPesquisa.innerHTML="Resultado da Pesquisa: "
 
-    const dgvContatos=document.createElement('div')
-    dgvContatos.setAttribute('id','dgvContatos')
+    if(TitleOfDgvExist==false){
+      createTitleOfDgv(resPesquisa)
+      const contato=document.createElement('div')
+      contato.setAttribute('class','contato')
 
-    const dgvTitulos=document.createElement('div')
-    dgvTitulos.setAttribute('class','dgvTitulos')
-
-    const c1=document.createElement('div')
-    c1.setAttribute('class','c1')
-    c1.innerHTML="Id";
-    dgvTitulos.appendChild(c1)
-    const c2=document.createElement('div')
-    c2.setAttribute('class','c2')
-    c2.innerHTML="Nome";
-    dgvTitulos.appendChild(c2)
-    const c3=document.createElement('div')
-    c3.setAttribute('class','c3')
-    c3.innerHTML="Celular";
-    dgvTitulos.appendChild(c3)
-    const c4=document.createElement('div')
-    c4.setAttribute('class','c4')
-    c4.innerHTML="E-mail";
-    dgvTitulos.appendChild(c4)
-    const c5=document.createElement('div')
-    c5.setAttribute('class','c5')
-    c5.innerHTML="Data de Nascimento";
-    dgvTitulos.appendChild(c5)
-  
-    dgvContatos.appendChild(dgvTitulos)
-
-    const contato=document.createElement('div')
-    contato.setAttribute('class','contato')
-
-    const idContato=document.createElement('p')
-    idContato.innerHTML=data[0].id;
-    idContato.setAttribute("class","c1");
-    contato.appendChild(idContato)
+      const idContato=document.createElement('p')
+        idContato.innerHTML=data[0].id;
+        idContato.setAttribute("class","c1");
+      contato.appendChild(idContato)
 
     const nomeContato=document.createElement('p')
-    nomeContato.innerHTML=data[0].nome;
-    nomeContato.setAttribute("class","c2");
+      nomeContato.innerHTML=data[0].nome;
+      nomeContato.setAttribute("class","c2");
     contato.appendChild(nomeContato)
 
     const celularContato=document.createElement('p')
-    celularContato.innerHTML=data[0].celular;
-    celularContato.setAttribute("class","c3");
+      celularContato.innerHTML=data[0].celular;
+      celularContato.setAttribute("class","c3");
     contato.appendChild(celularContato)
 
     const emailContato=document.createElement('p')
-    emailContato.innerHTML=data[0].email;
-    emailContato.setAttribute("class","c4");
+      emailContato.innerHTML=data[0].email;
+      emailContato.setAttribute("class","c4");
     contato.appendChild(emailContato)
 
     const dtnascContato=document.createElement('p')
-    dtnascContato.innerHTML=data[0].dtnasc;
-    dtnascContato.setAttribute("class","c5");
+      dtnascContato.innerHTML=data[0].dtnasc;
+      dtnascContato.setAttribute("class","c5");
+
     contato.appendChild(dtnascContato)
-
-    const btnLimparPesquisa = document.createElement('button');
-      btnLimparPesquisa.addEventListener('click', function() {
-        resPesquisa.innerHTML=''
-    });
-      btnLimparPesquisa.setAttribute('class',"btnlimpar")
-      btnLimparPesquisa.innerHTML = 'Limpar';
-
- 
-   
-    resPesquisa.appendChild(dgvContatos)
     resPesquisa.appendChild(contato)
-    resPesquisa.appendChild(btnLimparPesquisa)
+    }else{
+      const contato=document.createElement('div')
+      contato.setAttribute('class','contato')
 
+      const idContato=document.createElement('p')
+        idContato.innerHTML=data[0].id;
+        idContato.setAttribute("class","c1");
+      contato.appendChild(idContato)
+
+    const nomeContato=document.createElement('p')
+      nomeContato.innerHTML=data[0].nome;
+      nomeContato.setAttribute("class","c2");
+    contato.appendChild(nomeContato)
+
+    const celularContato=document.createElement('p')
+      celularContato.innerHTML=data[0].celular;
+      celularContato.setAttribute("class","c3");
+    contato.appendChild(celularContato)
+
+    const emailContato=document.createElement('p')
+      emailContato.innerHTML=data[0].email;
+      emailContato.setAttribute("class","c4");
+    contato.appendChild(emailContato)
+
+    const dtnascContato=document.createElement('p')
+      dtnascContato.innerHTML=data[0].dtnasc;
+      dtnascContato.setAttribute("class","c5");
+      
+    contato.appendChild(dtnascContato)
+    resPesquisa.appendChild(contato)
+    }
+
+    
+
+   
+    const btnLimparPesquisa = document.createElement('button');
+    btnLimparPesquisa.addEventListener('click', function() {
+      TitleOfDgvExist=false;
+      resPesquisa.innerHTML=''
+    });
+    btnLimparPesquisa.setAttribute('class',"btnlimpar")
+    btnLimparPesquisa.innerHTML = 'Limpar';
+    resPesquisa.appendChild(btnLimparPesquisa)
   })
 });
 
