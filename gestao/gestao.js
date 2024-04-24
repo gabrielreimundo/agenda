cfdea63b30e08df85de88915ca1ffc5eefe23f1f
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2024-04-23 21:24:30 -0300

    at

--- gestao/gestao.js
+++ gestao/gestao.js
@@ -1,42 +1,42 @@
 
-fetch('http://localhost:8081/dados', {
-  method: 'POST', // Especifica o método POST
-  headers: {
-    'Content-Type': 'application/json'
-  }
+
+const createDataGridView = function(el, dgvDados) {
+  const dgvLinha = document.createElement("div");
+  dgvLinha.setAttribute("class","dgvLinha");
+
+  const c1 = document.createElement("div");
+  c1.setAttribute("class","c1");
+  c1.innerHTML = el.id;
+  dgvLinha.appendChild(c1)
+
+  const c2 = document.createElement("div");
+  c2.setAttribute("class","c2");
+  c2.innerHTML = el.nome;
+  dgvLinha.appendChild(c2)
+            
+  const c3 = document.createElement("div");
+  c3.setAttribute("class","c3");
+  c3.innerHTML = el.celular;
+  dgvLinha.appendChild(c3)
+
+  const c4 = document.createElement("div");
+  c4.setAttribute("class","c4");
+  c4.innerHTML = el.email;
+  dgvLinha.appendChild(c4)
+
+  const c5 = document.createElement("div");
+  c5.setAttribute("class","c5");
+  c5.innerHTML = el.dtnasc;
+  dgvLinha.appendChild(c5)
+
+  dgvDados.appendChild(dgvLinha)
+}
+
+fetch('http://localhost:8081/dados')
+.then(res => res.json())
+.then(res => {
+  res.forEach(el => {
+    createDataGridView(el,dgvDados)
+    
+  })
 })
-.then(response => response.json())
-.then(data =>{
-    const infoCtt=document.getElementById('infoCtt')
-    const contato=document.createElement('div')
-    contato.setAttribute('class','contato')
-
-    const idContato=document.createElement('p')
-      idContato.innerHTML=data[0].id;
-      idContato.setAttribute("class","c1");
-    contato.appendChild(idContato)
-
-  const nomeContato=document.createElement('p')
-    nomeContato.innerHTML=data[0].nome;
-    nomeContato.setAttribute("class","c2");
-  contato.appendChild(nomeContato)
-
-  const celularContato=document.createElement('p')
-    celularContato.innerHTML=data[0].celular;
-    celularContato.setAttribute("class","c3");
-  contato.appendChild(celularContato)
-
-  const emailContato=document.createElement('p')
-    emailContato.innerHTML=data[0].email;
-    emailContato.setAttribute("class","c4");
-  contato.appendChild(emailContato)
-
-  const dtnascContato=document.createElement('p')
-    dtnascContato.innerHTML=data[0].dtnasc;
-    dtnascContato.setAttribute("class","c5");
-
-  contato.appendChild(dtnascContato)
-  infoCtt.appendChild(contato)
-
-})
-
